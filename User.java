2cf7b20e33e8236eed2a20f134c13c7ba18a40ed
alban <alban.haka@stud.unifi.com>
2025-01-17 19:50:45 +0100

    add class user

---
+++ User.java
@@ -0,0 +1,20 @@
+public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private String mail;
+    private String password;
+    private int age;
+
+    //costruttore
+    public User(int id, String name, String surname, String mail, String password, int age) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.mail = mail;
+        this.password = password;
+        this.age = age;
+    }
+
+
+}
