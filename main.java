b6ab13f926963ef2f2796016b1992a9ee1a4167f
Guido <guidor0033@gmail.com>
2025-01-24 14:11:10 +0100

    Change main

--- main.java
+++ main.java
@@ -1,10 +1,18 @@
 import src.managerdatabase.DBConnection;
 import src.managerdatabase.SetDB;
+import src.orm.CommunityDAO;
+
+import java.sql.SQLException;
+import java.util.Map;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         DBConnection.connect();
         SetDB.createDB();
+
+        CommunityDAO communityDAO = new CommunityDAO();
+        communityDAO.save(Map.of( "title", "WorldNews", "description","A place for major news from around the world" ));
+        communityDAO.findById(1).ifPresent(System.out::println);
         DBConnection.disconnect();
     }
 }
