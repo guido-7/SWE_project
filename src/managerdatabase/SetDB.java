fdc8046aa0d6e13e612e00f4d0f0b183eaef1668
Guido <guidor0033@gmail.com>
2025-01-24 11:30:29 +0100

    Change to SetDB

--- src/managerdatabase/SetDB.java
+++ src/managerdatabase/SetDB.java
@@ -1,25 +1,26 @@
 package src.managerdatabase;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+public class  SetDB {
+    public static void createDB(){
 
-public class SetDB {
-    public DBConnection DBconn;
-    //public static Connection conn;
+        createUserTable();
+        createCommunityTable();
+        createAdminTable();
+        createCommentTable();
+        createPostTable();
+        createBannedUsersTable();
+        createCommentHierarchyTable();
+        createModeratorTable();
+        createPostVotesTable();
+        createRulesTable();
+        createSubscriptionTable();
+        createUserAccessTable();
 
-    public static void main(String[] args) {
-        SetDB setDb = new SetDB();
-        Connection conn = DBConnection.connect();
-        setDb.createCommunityTable();
-
-        DBConnection.disconnect();
     }
 
-
-    public void createCommunityTable() {
+    public static  void createCommunityTable() {
         String sql = "CREATE TABLE IF NOT EXISTS Community ("
-                + " id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + " id SERIAL PRIMARY KEY,"
                 + " title TEXT NOT NULL,"
                 + " description TEXT"
                 + ");";
@@ -27,23 +28,23 @@ public class SetDB {
         DBConnection.query(sql);
     }
 
-    public void createUserTable() {
+    public static void createUserTable() {
         String sql = "CREATE TABLE IF NOT EXISTS User ("
-                + " id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + " name TEXT,"
-                + " surname TEXT,"
-                + " password TEXT NOT NULL,"
-                + " authen BOOLEAN"
+                + " id SERIAL PRIMARY KEY,"
+                + " nickname TEXT NOT NULL,"
+                + " name TEXT NOT NULL,"
+                + " surname TEXT NOT NULL"
                 + ");";
 
         DBConnection.query(sql);
     }
 
-    public void createUserAccessTable() {
+    public static void  createUserAccessTable() {
         String sql = "CREATE TABLE IF NOT EXISTS UserAccess ("
-                + " id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + " email TEXT PRIMARY KEY,"
                 + " user_id INTEGER,"
-                + " last_login TIMESTAMP,"
+                + " password TEXT,"
+                + " authen TEXT,"
                 + " FOREIGN KEY (user_id) REFERENCES User(id),"
                 + " UNIQUE(user_id)"
                 + ");";
@@ -51,7 +52,7 @@ public class SetDB {
         DBConnection.query(sql);
     }
 
-    public void createAdminTable() {
+    public static void createAdminTable() {
         String sql = "CREATE TABLE IF NOT EXISTS Admin ("
                 + " id INTEGER PRIMARY KEY,"
                 + " FOREIGN KEY (id) REFERENCES User(id)"
@@ -60,7 +61,7 @@ public class SetDB {
         DBConnection.query(sql);
     }
 
-    public void createRulesTable() {
+    public static void createRulesTable() {
         String sql = "CREATE TABLE IF NOT EXISTS Rules ("
                 + " id INTEGER,"
                 + " community_id INTEGER,"
@@ -72,10 +73,10 @@ public class SetDB {
         DBConnection.query(sql);
     }
 
-    public void createPostTable() {
+    public static void createPostTable() {
         String sql = "CREATE TABLE IF NOT EXISTS Post ("
-                + " id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + " orario TIMESTAMP,"
+                + " id UUID PRIMARY KEY,"
+                + " time TIMESTAMP,"
                 + " likes INTEGER DEFAULT 0,"
                 + " dislikes INTEGER DEFAULT 0,"
                 + " content TEXT NOT NULL,"
@@ -88,35 +89,39 @@ public class SetDB {
         DBConnection.query(sql);
     }
 
-    public void createCommentTable() {
+    public static void createCommentTable() {
         String sql = "CREATE TABLE IF NOT EXISTS Comment ("
-                + " id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + " orario TIMESTAMP,"
-                + " likes INTEGER DEFAULT 0,"
-                + " dislikes INTEGER DEFAULT 0,"
-                + " content TEXT NOT NULL,"
-                + " user_id INTEGER NOT NULL,"
-                + " post_id INTEGER NOT NULL,"
-                + " FOREIGN KEY (user_id) REFERENCES User(id),"
-                + " FOREIGN KEY (post_id) REFERENCES Post(id)"
+                + " id SERIAL,"  // Use SERIAL for auto-incremented integers
+                + " post_id UUID,"  // UUID type for post_id
+                + " time TIMESTAMP,"  // Timestamp for the comment
+                + " likes INTEGER DEFAULT 0,"  // Default value for likes
+                + " dislikes INTEGER DEFAULT 0,"  // Default value for dislikes
+                + " content TEXT NOT NULL,"  // Content of the comment
+                + " user_id INTEGER NOT NULL,"  // User id for the commenter
+                + " PRIMARY KEY (id, post_id),"  // Composite primary key (id, post_id)
+                + " FOREIGN KEY (user_id) REFERENCES User(id),"  // Foreign key for user reference
+                + " FOREIGN KEY (post_id) REFERENCES Post(id)"  // Foreign key for post reference
                 + ");";
 
         DBConnection.query(sql);
     }
 
-    public void createCommentHierarchyTable() {
+    public static void createCommentHierarchyTable() {
         String sql = "CREATE TABLE IF NOT EXISTS CommentHierarchy ("
-                + " parent_id INTEGER,"
-                + " child_id INTEGER,"
-                + " PRIMARY KEY (parent_id, child_id),"
-                + " FOREIGN KEY (parent_id) REFERENCES Comment(id),"
-                + " FOREIGN KEY (child_id) REFERENCES Comment(id)"
+                + " parent_id INTEGER, "
+                + " post_id UUID, "
+                + " child_id INTEGER, "
+                + " PRIMARY KEY (parent_id, child_id,post_id), "
+                + " FOREIGN KEY (parent_id) REFERENCES Comment(id), "
+                + " FOREIGN KEY (child_id) REFERENCES Comment(id) "
+                + " FOREIGN KEY (post_id) REFERENCES Post(id) "
                 + ");";
 
         DBConnection.query(sql);
     }
 
-    public void createBannedUsersTable() {
+
+    public static void  createBannedUsersTable() {
         String sql = "CREATE TABLE IF NOT EXISTS BannedUsers ("
                 + " user_id INTEGER,"
                 + " community_id INTEGER,"
@@ -130,7 +135,7 @@ public class SetDB {
         DBConnection.query(sql);
     }
 
-    public void createModeratorTable() {
+    public static void  createModeratorTable() {
         String sql = "CREATE TABLE IF NOT EXISTS Moderator ("
                 + " user_id INTEGER,"
                 + " community_id INTEGER,"
@@ -144,7 +149,7 @@ public class SetDB {
         DBConnection.query(sql);
     }
 
-    public void createSubscriptionTable() {
+    public static void  createSubscriptionTable() {
         String sql = "CREATE TABLE IF NOT EXISTS Subscription ("
                 + " user_id INTEGER,"
                 + " community_id INTEGER,"
@@ -157,10 +162,10 @@ public class SetDB {
         DBConnection.query(sql);
     }
 
-    public void createPostVotesTable() {
+    public static void  createPostVotesTable() {
         String sql = "CREATE TABLE IF NOT EXISTS PostVotes ("
                 + " user_id INTEGER,"
-                + " post_id INTEGER,"
+                + " post_id UUID,"
                 + " vote_type TEXT CHECK(vote_type IN ('like', 'dislike')),"
                 + " PRIMARY KEY (user_id, post_id),"
                 + " FOREIGN KEY (user_id) REFERENCES User(id),"
