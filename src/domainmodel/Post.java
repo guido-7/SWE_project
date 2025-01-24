21ac0376e66dcae8dda76179c5da0c41239a13ee
alban <alban.haka@stud.unifi.com>
2025-01-24 18:43:12 +0100

    Implemented attributes Post class

--- src/domainmodel/Post.java
+++ src/domainmodel/Post.java
@@ -1,4 +1,26 @@
 package src.domainmodel;
 
+import java.sql.Timestamp;
+import java.util.UUID;
+
 public class Post {
+    private UUID id;
+    private Timestamp time;
+    private int likes;
+    private int dislikes;
+    private String content;
+    private int userId;
+    private int communityId;
+
+    public Post(UUID id, Timestamp time, String content, int userId, int communityId) {
+        this.id = id;
+        this.time = time;
+        this.likes = 0;
+        this.dislikes = 0;
+        this.content = content;
+        this.userId = userId;
+        this.communityId = communityId;
+    }
+
+
 }
