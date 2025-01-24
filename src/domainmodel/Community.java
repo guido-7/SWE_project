710a80359275cecb6619ca26b01f1e5776ed5066
Guido <guidor0033@gmail.com>
2025-01-24 14:10:42 +0100

    Change Community

--- src/domainmodel/Community.java
+++ src/domainmodel/Community.java
@@ -1,4 +1,34 @@
 package src.domainmodel;
 
 public class Community {
+    private final int id;
+    private String title;
+    private String description;
+
+    public Community(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    //getters and setters
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
