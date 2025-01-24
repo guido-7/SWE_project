e8df6d986df0ef56ba2706a6afdd165b4c58a9f6
Guido <guidor0033@gmail.com>
2025-01-24 11:29:22 +0100

    add BaseDao

--- src/managerdatabase/BaseDao.java
+++
@@ -1,4 +0,0 @@
-package src.managerdatabase;
-
-public class BaseDao {
-}
--- src/orm/BaseDAO.java
+++ src/orm/BaseDAO.java
@@ -1,4 +1,74 @@
 package src.orm;
 
-public class BaseDAO {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public abstract class BaseDAO<T, ID> {
+    public BaseDAO() {
+    }
+
+    public Optional<T> findById(ID id) throws SQLException {
+        String query = getFindByIdQuery();
+        try (Connection connection = DBConnection.connect();
+            PreparedStatement statement = connection.prepareStatement(query)) {
+            setFindByIdParams(statement, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return Optional.of(mapResultSetToEntity(resultSet));
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    /*
+    public void save(T entity) throws SQLException {
+        String query = getInsertQuery();
+        try (Connection connection = DBConnection.connect();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            setInsertParams(statement, entity);
+            statement.executeUpdate();
+        }
+    }
+
+    public void update(T entity) throws SQLException {
+        String query = getUpdateQuery();
+        try (Connection connection = DBConnection.connect();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            setUpdateParams(statement, entity);
+            statement.executeUpdate();
+        }
+    }
+
+    public void deleteById(ID id) throws SQLException {
+        String query = getDeleteQuery();
+        try (Connection connection = DBConnection.connect();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            setDeleteParams(statement, id);
+            statement.executeUpdate();
+        }
+    }
+    */
+
+    protected abstract String getFindByIdQuery();
+
+    protected abstract void setFindByIdParams(PreparedStatement statement, ID id) throws SQLException;
+
+    //protected abstract String getFindAllQuery();
+
+    //protected abstract String getInsertQuery();
+
+    //protected abstract void setInsertParams(PreparedStatement statement, T entity) throws SQLException;
+
+    //protected abstract String getUpdateQuery();
+
+    //protected abstract void setUpdateParams(PreparedStatement statement, T entity) throws SQLException;
+
+    //protected abstract String getDeleteQuery();
+
+    //protected abstract void setDeleteParams(PreparedStatement statement, ID id) throws SQLException;
+
+    protected abstract T mapResultSetToEntity(ResultSet resultSet) throws SQLException;
 }
