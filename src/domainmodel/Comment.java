6782515b7aae133a863b96d808f88cc7195dc4c1
alban <alban.haka@stud.unifi.com>
2025-01-24 18:37:16 +0100

    add class Comment.java

---
+++ src/domainmodel/Comment.java
@@ -0,0 +1,17 @@
+package src.domainmodel;
+
+import java.time.LocalDateTime;
+
+public class Comment {
+    private int id;
+    private String post_id;
+    private int user_id;
+    private LocalDateTime time;
+    private String content;
+    private int upvotes;
+    private int downvotes;
+
+
+
+
+}
