ba366864cca6fff2c9c781245384db6c6671d98e
Guido <guidor0033@gmail.com>
2025-01-24 14:11:00 +0100

    Change CommunityDAO

--- src/orm/CommunityDAO.java
+++ src/orm/CommunityDAO.java
@@ -8,7 +8,7 @@ public class CommunityDAO extends BaseDAO<Community, Integer> {
 
     @Override
     protected String getFindByIdQuery() {
-        return "SELECT * FROM community WHERE id = ?";
+        return "SELECT * FROM Community WHERE id = ?";
     }
 
     @Override
@@ -18,7 +18,43 @@ public class CommunityDAO extends BaseDAO<Community, Integer> {
 
     @Override
     protected Community mapResultSetToEntity(ResultSet resultSet) throws SQLException {
-        Community community = new Community();
-        return community;
+        int id = resultSet.getInt("id");
+        String title = resultSet.getString("title");
+        String description = resultSet.getString("description");
+        return new Community(id, title, description);
     }
+
+    @Override
+    protected String getInsertQuery() {
+        return "INSERT INTO Community ( title, description) VALUES ( ?, ?)";
+    }
+    // CommunityDAO.save(Community(id,title,description))
+    @Override
+    protected void setInsertParams(PreparedStatement statement, Map<String,Object> parameters) throws SQLException {
+        statement.setString(1, (String) parameters.get("title"));
+        statement.setString(2, (String) parameters.get("description"));
+    }
+
+    @Override
+    protected String getUpdateQuery() {
+        return "UPDATE Community SET title = ?, description = ? WHERE id = ?";
+    }
+
+    @Override
+    protected void setUpdateParams(PreparedStatement statement, Community entity) throws SQLException {
+        statement.setString(1, entity.getTitle());
+        statement.setString(2, entity.getDescription());
+        statement.setInt(3, entity.getId());
+    }
+
+    @Override
+    protected String getDeleteQuery() {
+        return "DELETE FROM Community WHERE id = ?";
+    }
+
+    @Override
+    protected void setDeleteParams(PreparedStatement statement, Integer id) throws SQLException {
+        statement.setInt(1, id);
+    }
+
 }
