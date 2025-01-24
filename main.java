07359914267bbb6d94dd8e4f7069adb27cc796ce
Guido <guidor0033@gmail.com>
2025-01-24 11:33:06 +0100

    Changed main for run SetDB

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
+import src.managerdatabase.DBConnection;
+import src.managerdatabase.SetDB;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World !!");
+        DBConnection.connect();
+        SetDB.createDB();
+        DBConnection.disconnect();
     }
 }
