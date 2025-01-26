28aa0a73b8f63e081b80c08125ae16eebaf9c41d
Guido <guidor0033@gmail.com>
2025-01-26 18:51:47 +0100

    change attribute

--- src/domainmodel/Comment.java
+++ src/domainmodel/Comment.java
@@ -4,23 +4,41 @@ import java.time.LocalDateTime;
 
 public class Comment {
     private final int id;
-    private final Post post;
-    private final User user;
+    private Post post;
+    private User user;
+    private final int  post_id;
+    private final int user_id;
     private final LocalDateTime time;
     private String content;
     private int likes = 0;
     private int dislikes = 0;
     private boolean is_modified = false;
 
-    public Comment(int id, Post post, User user, LocalDateTime time, String content, int likes, int dislikes, boolean is_modified) {
+    public Comment(int id, int post_id, int user_id, LocalDateTime time, String content, int likes, int dislikes, boolean is_modified) {
         this.id = id;
-        this.post = post;
-        this.user = user;
+        this.post_id = post_id;
+        this.user_id = user_id;
         this.time = time;
         this.content = content;
         this.likes = likes;
         this.dislikes = dislikes;
         this.is_modified = is_modified;
+    }
+
+    public int getPost_id() {
+        return post_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public int getId() {
