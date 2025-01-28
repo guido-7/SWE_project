dad4e7da252d3714ba3c34659b23e0f7bd5e6a60
Guido <guidor0033@gmail.com>
2025-01-28 17:40:14 +0100

    Add level attribute

--- src/domainmodel/Comment.java
+++ src/domainmodel/Comment.java
@@ -7,16 +7,18 @@ public class Comment {
     private Post post;
     private User user;
     private final int  post_id;
+    private final int level;
     private final int user_id;
-    private final LocalDateTime time;
     private String content;
     private int likes = 0;
     private int dislikes = 0;
+    private final LocalDateTime time;
     private boolean is_modified = false;
 
-    public Comment(int id, int post_id, int user_id, LocalDateTime time, String content, int likes, int dislikes, boolean is_modified) {
+    public Comment(int id, int post_id,int level, int user_id, String content, int likes, int dislikes, LocalDateTime time, boolean is_modified) {
         this.id = id;
         this.post_id = post_id;
+        this.level = level;
         this.user_id = user_id;
         this.time = time;
         this.content = content;
