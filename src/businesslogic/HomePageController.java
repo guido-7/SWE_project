64c6311c65c2c50aa059456f6e73c1d3599ff69d
Guido <guidor0033@gmail.com>
2025-02-04 18:04:53 +0100

    Add read post by db for homepage

--- src/businesslogic/HomePageController.java
+++ src/businesslogic/HomePageController.java
@@ -20,9 +20,8 @@ public class HomePageController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        posts = new ArrayList<>(getPosts());
         try{
+            posts = new ArrayList<>(getPosts());
             for (Post post : posts) {
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/resources/fxml/Post.fxml"));
                 VBox vBox = fxmlLoader.load();
@@ -35,13 +34,12 @@ public class HomePageController implements Initializable {
         }
     }
 
-    private List<Post> getPosts(){
-        //PostController postController = new PostController();
+    private List<Post> getPosts() throws SQLException {
+        PostController postController = new PostController();
         List<Post> ls = new ArrayList<>();
 
-        for (int i = 0; i < 8; i++) {
-            ls.add(new Post("title ", "Content " + i, 0));
-            //ls.add(postController.getPost(i));
+        for (int i = 1; i < 8; i++) {
+            ls.add(postController.getPost(i));
         }
 
         return ls;
