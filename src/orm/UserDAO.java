0804a2d6118756cc4dbf55a0450fd1143f93a115
alban <alban.haka@stud.unifi.com>
2025-02-06 23:41:46 +0100

    fix LoginPage sistem

--- src/orm/UserDAO.java
+++ src/orm/UserDAO.java
@@ -1,7 +1,9 @@
 package src.orm;
 
 import src.domainmodel.*;
+import src.managerdatabase.DBConnection;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -63,4 +65,21 @@ public class UserDAO extends BaseDAO<User,Integer> {
         Set<Permits> permits = PermitsManager.createAdminPermits();  // Assuming permits need to be fetched from another table
         return new Admin(id, nickname, name, surname, permits);
     }
+
+    public boolean isValidUser(String email, String password) throws SQLException {
+        String query = "SELECT COUNT(*) FROM UserAccess WHERE email = ? AND password = ?";
+        try (Connection connection = DBConnection.open_connection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, email);
+            statement.setString(2, password);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next() && resultSet.getInt(1) > 0) {
+                    return true; // Email e password corrette
+                }
+            }
+        }
+        return false; // Credenziali errate
+    }
+
+
 }
