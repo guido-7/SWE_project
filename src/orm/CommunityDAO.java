df5319625b1b819a7ed56f4ca56b977e19163da9
Andrea <innocentiandrea6@gmail.com>
2025-02-19 10:50:34 +0100

    added new robust logging during research of title from the searchbar

--- src/orm/CommunityDAO.java
+++ src/orm/CommunityDAO.java
@@ -118,7 +118,7 @@ public class CommunityDAO extends BaseDAO<Community, Integer> {
                 }
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("No title found with this query");
         }
         return communities;
     }
