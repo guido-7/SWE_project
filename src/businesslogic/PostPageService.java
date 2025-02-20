e5fabe6c2fd580cf9148e6ce525bcf2f60d2ce2c
Guido <guidor0033@gmail.com>
2025-02-20 17:59:49 +0100

    add PostPageService

---
+++ src/businesslogic/PostPageService.java
@@ -0,0 +1,11 @@
+package src.businesslogic;
+
+import src.domainmodel.Post;
+
+public class PostPageService {
+    Post post;
+
+    public PostPageService(Post post) {
+        this.post = post;
+    }
+}
