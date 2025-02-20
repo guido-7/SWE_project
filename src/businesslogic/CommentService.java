69dc835282d8ef4e501ec3a22ff3bff4a8b4e0c6
Guido <guidor0033@gmail.com>
2025-02-20 02:13:20 +0100

    add CommentService for managing comment data and retrieval

---
+++ src/businesslogic/CommentService.java
@@ -0,0 +1,45 @@
+package src.businesslogic;
+
+import src.domainmodel.Comment;
+import src.domainmodel.Post;
+import src.orm.CommentDAO;
+import src.orm.UserDAO;
+
+import java.util.List;
+
+public class CommentService {
+    Comment comment;
+    UserDAO userDAO = new UserDAO();
+    CommentDAO commentDAO = new CommentDAO();
+
+    int noOfCommentsTaken = 0;
+    int numberOfComments = 10;
+
+    public CommentService(Comment comment) {
+        this.comment = comment;
+    }
+
+    public Comment getComment() {
+        return comment;
+    }
+
+    public String getCommentText() {
+        return comment.getContent();
+    }
+
+    public String getCommentAuthor() {
+        return userDAO.getNicknameById(comment.getUser_id());
+    }
+
+    public List<Comment> getRootComments(Post post) {
+        List<Comment> rootComments = commentDAO.getRootComments(post, numberOfComments, 0);
+        System.out.println("Post ID: " + post.getId());
+        noOfCommentsTaken = rootComments.size();
+        return rootComments;
+    }
+
+    public List<Post> getCommentsByLevel(){
+        return  null;
+    }
+
+}
