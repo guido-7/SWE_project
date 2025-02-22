233158aea19fae41a774f7c3bfc90e1ccdd925e5
Guido <guidor0033@gmail.com>
2025-02-22 14:23:53 +0100

    enhance CommentController to support sub-comments and improve initialization logic

--- src/controllers/CommentController.java
+++ src/controllers/CommentController.java
@@ -1,13 +1,22 @@
 package src.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import src.businesslogic.CommentService;
 import src.domainmodel.Comment;
 import src.servicemanager.FormattedTime;
 
-public class CommentController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class CommentController implements Controller, Initializable {
     @FXML
     private Label content;
     @FXML
@@ -20,12 +29,38 @@ public class CommentController {
     private Label scoreLabel;
     @FXML
     private Label username;
+    @FXML
+    private ImageView moreComments;
+    @FXML
+    private ImageView minusComments;
+    @FXML
+    private VBox repliesContainer;
 
-    private CommentService commentService;
+    private final CommentService commentService;
     private final FormattedTime formatter = new FormattedTime();
 
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        moreComments.setVisible(false);
+        minusComments.setVisible(false);
+        init_data();
+
+        moreComments.setOnMouseClicked(event -> {
+            moreComments.setVisible(false);
+            minusComments.setVisible(true);
+            List<Comment> subComment = commentService.getCommentsByLevel();
+            loadSubComments();
+        } );
+
+        minusComments.setOnMouseClicked(event -> {
+            minusComments.setVisible(false);
+            moreComments.setVisible(true);
+            repliesContainer.getChildren().clear();
+        });
     }
 
     public void setData(Comment comment) {
@@ -36,4 +71,28 @@ public class CommentController {
         date.setText(formatter.getFormattedTime(comment.getTime()));
         scoreLabel.setText(comment.getLikes() - comment.getDislikes() + "");
     }
+
+    @Override
+    public void init_data() {
+        if(commentService.hasSubComments()) {
+            moreComments.setVisible(true);
+        }
+    }
+
+    private void loadSubComments() {
+        List<Comment> subComments = commentService.getCommentsByLevel();
+        for (Comment comment : subComments) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/src/view/fxml/Comment.fxml"));
+                CommentController commentController = new CommentController(new CommentService(comment));
+                fxmlLoader.setController(commentController);
+                VBox commentBox = fxmlLoader.load();
+                commentController.setData(comment);
+                repliesContainer.getChildren().add(commentBox);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
